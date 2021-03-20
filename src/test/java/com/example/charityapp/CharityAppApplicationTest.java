fd6d6d48a67d7ada128e80e94b9f04d98cffb41c
Zukkari <stanislav.myshkovski@gmail.com>
2021-03-20 22:34:20 +0200

    Preparations for integration tests
    - Add H2 for in-memory DB for testing

---
+++ src/test/java/com/example/charityapp/CharityAppApplicationTest.java
@@ -0,0 +1,11 @@
+package com.example.charityapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharityAppApplicationTest {
+
+  @Test
+  void context_loads() {}
+}
